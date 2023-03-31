9c57fd94b48ee37246f6efc854a88096b762b2bd
jvasko <judit.vaskone@gmail.com>
2023-03-31 17:49:12 +0200

    initial commit

---
+++ src/app/nav/navbar.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavbarComponent {
+    searchTerm : string = "";
+//    foundRecipess : IRecipe[] = [];
+  
+    searchRecipe(searchTerm:string) {
+        // this.eventService.searchRecipes(searchTerm).subscribe(
+        //     recipes => {
+        //       this.foundRecipes = recipes;
+        //     }
+        // );
+    }
+
+}
+
