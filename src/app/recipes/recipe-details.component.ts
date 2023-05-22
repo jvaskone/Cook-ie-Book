cfe4257a606639341a4627b8bd5c5f2e85952ea7
jvasko <judit.vaskone@gmail.com>
2023-05-22 15:26:44 +0200

    #865c1p0x1 recipes can be edited and deleted as well

--- src/app/recipes/recipe-details.component.ts
+++ src/app/recipes/recipe-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { RecipeService } from './shared/recipe.service';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute, Params, Router } from '@angular/router';
 import { IRecipe } from './shared/recipes.model';
 
 @Component({
@@ -16,7 +16,8 @@ export class RecipeDetailsComponent implements OnInit {
 
   constructor(
     private recipeService: RecipeService,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private router:Router
   ) {}
 
   /*ngOnInit() {
@@ -43,6 +44,20 @@ export class RecipeDetailsComponent implements OnInit {
       return "";
     }
     return this.recipeService.getCategory(recipe.categoryId)?.name;
+  }
+
+  deleteRecipe(): void {
+    if (this.recipe == null) {
+      //       
+    } else {
+      if (confirm(`Biztosan törlöd a receptet: ${this.recipe.name}?`)) {
+         this.recipeService.deleteRecipe(this.recipe.id).subscribe({
+          //  next: () => this.onSaveComplete(`${this.recipe?.name} törölve.`),
+          //  error: err => this.errorMessage = err
+         });
+      }
+    }
+    this.router.navigate(['/recipes']);
   }
 
 }
