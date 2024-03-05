e967ef73ff914ba9981be79f5b78d5a305e3e027
Judit Vasko-Szedlar <129600562+jvaskone@users.noreply.github.com>
2024-03-05 11:42:53 +0100

    sample data added

--- src/app/recipes/shared/demo-recipe.service.ts
+++ src/app/recipes/shared/demo-recipe.service.ts
@@ -114,5 +114,15 @@ const RECIPES: IRecipe[] = [
     image: "",
     "ingredients": "12 db Bodzavirág\n1 dl Citromlé\n3 szál Menta\n15 dkg Cukor\n2 ek Méz\n6 dl Víz",
     "instructions": "A bodzavirágokat alaposan átnézzük, hogy biztosan nincs-e rajta nem kívánt élősködő, majd egy nagy tálba tesszük.\n\nA bodzához adjuk a karikára vágott citromot és a letépkedett mentaleveleket.\n\nAz egészet nyakon ötjük 6 dl forró vízzel, és egy éjszakán át letakarva állni hagyjuk.\n\nMásnap átszűrjük, és egy edényben közepes lángon melegíteni kezdjük. Hozzáadjuk a cukrot és mézet, majd addig melegítjük, amíg teljesen el nem olvad. Érdemes utána megkóstolni, hogy elég édes-e. Azt vegyünk figyelembe, hogy mivel lefagyasztjuk, így édesebbnek kell lennie, mint egy normál pohár bodzaszörpnek, mert a hideg miatt kevésbé fogjuk élénken érezni az ízeket.\n\nHa elkészült, akkor félretesszük hűlni, és ha már szobahőmérsékletű, akkor a jégkrémformákat megtöltjük a bodzás vízzel, és mélyhűtőbe tesszük."
+  },
+  {
+    "id": 23,
+    "name": "Eperkrémes torta",
+    "categoryId": 5,    
+    category: {id: 5, name: "Torta" },
+    imageUrl: "",
+    image: "",
+    "ingredients": "8 tojás\n8 evőkanál kristálycukor\n8 evőkanál liszt\nkéshegynyi szódabikarbóna\n\nA krémhez:\n30 dkg eper\n25 dkg vaj\n25 dkg porcukor\n5 dl tej\n5 evőkanál liszt\n1 evőkanál citromlé\n1 csomag vaníliás cukor\n2 dl habtejszín\n\nA tetejére:\n1 csomag express zselatinfix\nEper szemek\n\nA torta oldala bevonásához:\n2,5 dl habtejszín",
+    "instructions": "A piskótalaphoz a tojásokat szétválasztjuk, majd a tojásfehérjét kemény habbá verjük, közben kanalanként hozzáadjuk a cukrot. Ezután jöhet hozzá a tojássárgája, valamint a szódabikarbónával elkevert liszt. A kapott masszát 3 részre osztjuk és külön sütjük meg a lapokat\n\nA krémhez a tűzhelyen felmelegítjük a fél liter tejet, és kézi habverővel, folyamatos kevergetés mellett hozzáadagoljuk a lisztet . Addig kevergetjük, míg jó sűrű masszává nem válik. Ha ez megtörtént, hűlni hagyjuk.\n\nA vajat, a porcukrot, valamint a vaníliás cukrot egy tálban csomómentes krémmé keverjük, majd hozzáadjuk a kihűlt tejes masszát. Az eperből pár szemet félreteszünk a díszítéshez, a többit nagyon apró darabokra vágjuk, és belekeverjük a krémbe, végül hozzákeverjük a citromlét is.\n\nA tejszínt kemény habbá verjük és hozzá keverjük a krémhez.\n\nÖsszeállításkor a krémet 3 részre osztjuk, 1-1 részt rákenünk 1-1 tortalapra. A két tortalapot egymásra tesszük, és befedjük a harmadik tortalappal. A torta oldalát és a tetejét bevonjuk a maradék krémmel.\n\nA tetejét kirakjuk eperrel, majd a zselatinnal leöntjük.\n\nA zselatint a tasakon található használati utasítás szerint oldjuk fel.\n\nA tejszínt kemény habbá verjük és a torta oldalát bevonjuk."
   }
 ];
