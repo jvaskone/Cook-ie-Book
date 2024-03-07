05504588cc7aace3039d6192f9426d0f82d44e2d
Judit Vasko-Szedlar <129600562+jvaskone@users.noreply.github.com>
2024-03-07 15:19:59 +0100

    correctiong default image data and validation

--- src/app/recipes/create-recipe.component.ts
+++ src/app/recipes/create-recipe.component.ts
@@ -35,7 +35,7 @@ export class CreateRecipeComponent implements OnInit {
       recipeCategory: ['', Validators.required],
       recipeIngredients: ['', Validators.required],
       recipeInstructions: ['', Validators.required],
-      recipeImageUrl: ['', Validators.required],
+      recipeImageUrl: [null],
       recipeImage: [null],
       recipeId: [0]
     });
@@ -125,9 +125,70 @@ export class CreateRecipeComponent implements OnInit {
     this.newRecipe.image = this.imageData;
     this.newRecipe.name = this.recipeForm.get('recipeName')?.value;
     this.newRecipe.categoryId = this.recipeForm.get('recipeCategory')?.value;
+    let category = this.recipeService.getCategory(this.newRecipe.categoryId);
+    if (category) {
+      this.newRecipe.category = category;
+    }    
     this.newRecipe.ingredients = this.recipeForm.get('recipeIngredients')?.value;
     this.newRecipe.instructions = this.recipeForm.get('recipeInstructions')?.value;
     this.newRecipe.imageUrl = this.recipeForm.get('recipeImageUrl')?.value;
+    if(!this.newRecipe.imageUrl && ! this.newRecipe.image) {
+      let cat = this.newRecipe.categoryId;      
+      switch (+cat) {
+          case 1: {
+            this.newRecipe.imageUrl = "assets/images/leves.jpg";            
+            break;
+          }
+          case 2: {
+            this.newRecipe.imageUrl = "assets/images/foetel.jpg";
+            break;
+          }
+          case 3: {
+            this.newRecipe.imageUrl = "assets/images/fozelek.jpg";
+            break;
+          }
+          case 4: {
+            this.newRecipe.imageUrl = "assets/images/suti.jpg";
+            break;
+          }
+          case 5: {
+            this.newRecipe.imageUrl = "assets/images/torta.jpg";
+            break;
+          }
+          case 6: {
+            this.newRecipe.imageUrl = "assets/images/salata.jpg";
+            break;
+          }
+          case 7: {
+            this.newRecipe.imageUrl = "assets/images/befott.jpg";
+            break;          
+          }
+        }                 
+    }
+    if(this.newRecipe.image == null) {
+      this.loadImageToData(this.newRecipe);
+    }
+    console.log(this.newRecipe);
+  }
+
+  loadImageToData(recipe: IRecipe) {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', recipe.imageUrl, true);
+    xhr.responseType = 'blob';
+    xhr.onload = () => {
+      if (xhr.status === 200) {
+        const blob = xhr.response as Blob;
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          const base64Data = reader.result as string;          
+          recipe.image = base64Data;          
+        };
+        reader.readAsDataURL(blob);
+      } else {
+        console.error('Error fetching image data:', xhr.statusText);
+      }
+    };
+    xhr.send();
   }
 
   cancel() {
@@ -140,14 +201,15 @@ export class CreateRecipeComponent implements OnInit {
     }
   }
 
-  addFile(event: any) {
+  addFile(event: any) {    
     if (event.target.files && event.target.files[0]) {
         var reader = new FileReader();
         reader.onload = (event: any) => {
           this.imageData = event.target.result;
         }
-        reader.readAsDataURL(event.target.files[0]);        
-      }
+        reader.readAsDataURL(event.target.files[0]);  
+        console.log(event.target.files[0]); 
+      }      
 }
 }
 
